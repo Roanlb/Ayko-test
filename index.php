739e30ccb250bd286ed925f28568890ac9c24006
Roanlb <roanlb@hotmail.co.uk>
2020-03-26 23:16:36 +0000

    add sort by selector

--- index.php
+++ index.php
@@ -32,7 +32,62 @@ $data = json_decode($data_json);
  }
  </script>
 
- <!-- 33 to 94 -->
+ <script>
+ function sortDatasets() {
+   const selector = document.getElementById('selector');
+   let datasetsArray = data;
+   if (selector.value == 'modified') {
+     datasetsArray = datasetsArray.sort((a, b) => {
+       return new Date(a.modified) - new Date(b.modified);
+     });
+   }
+   else if (selector.value == 'temporal') {
+     datasetsArray = datasetsArray.sort((a, b) => {
+       return new Date(a.temporal.toString().substring(0, 10)) - new Date(b.temporal.toString().substring(0, 10));
+     }); 
+   }
+   if (selector.value != 'Sort by') {
+
+     for (let i = 0; i < datasetsArray.length; i++) {
+       let idNo = `dataset${i}`;
+       let listItem = document.getElementById(idNo);
+        let coordinatesText = '';
+
+            if (Array.isArray(datasetsArray[i].spatial.coordinates[0])) {
+             
+              datasetsArray[i].spatial.coordinates[0].forEach(coordinatePair => {
+                coordinatesText += '[ ';
+                coordinatePair.forEach(coordinate => {
+                  coordinatesText += `${coordinate} `;
+                })
+                coordinatesText += '] ';
+              })
+            }
+            else {
+              datasetsArray[i].spatial.coordinates.forEach(coordinate => {
+                coordinatesText += `[${coordinate}] `;
+              })
+            }       
+
+       listItem.innerHTML = `<h3>${datasetsArray[i].title}</h3>
+        <h4><button id='description-button${i}' class='description-button' onClick='displayDescription(${i})'>Show description</button></h4>
+         <p class='description' id=${i}>${datasetsArray[i].description}</p>
+          <h5 class='identifier'>Identifier: ` + datasetsArray[i].identifier.toString().substr(-7) + ` </h5> 
+          <h5>Date modified: ${datasetsArray[i].modified}</h5> 
+          <h5>Publisher/Point of contact: ${datasetsArray[i].contactpoint_fn}</h5> 
+          <h5>Temporal: ` + datasetsArray[i].temporal.toString().slice(0, 9) + datasetsArray[i].temporal.toString().slice(18, -9) + `</h5>
+          <h5>Spatial coordinates: ${coordinatesText} </h5>`;
+      }
+     }
+   }
+
+</script>
+
+<select onChange='sortDatasets()' id='selector'>
+  <option>Sort by</option>
+  <option value='modified'>Modified</option>
+  <option value='temporal'>Temporal</option>
+</select>
 
  <ul id="initialDatasets">
 
