08fbbe3ffd4c2549cbd194e47ff62f4d644443a3
Roanlb <roanlb@hotmail.co.uk>
2020-03-26 23:12:41 +0000

    add list with json info

--- index.php
+++ index.php
@@ -22,3 +22,36 @@ $data = json_decode($data_json);
 <body>
 <img class='icon' src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/NOAA_logo.svg/1200px-NOAA_logo.svg.png" alt="The logo for the National Oceanic and Atmospheric Administration" height="60px" width="60px"><h1>National Oceanic and Atmospheric Administration Datasets</h1>
 <p class="universals">All contacts can be reached by email at NODC.Services@noaa.gov. All accrual periodicities are irregular.</p>
+
+  <script>
+ const data = <?php echo $data_json ?>
+
+ function displayDescription(id) {
+   const description = document.getElementById(id);
+   description.style.display === 'inline' ? description.style.display = 'none' :  description.style.display = 'inline';
+ }
+ </script>
+
+ <!-- 33 to 94 -->
+
+ <ul id="initialDatasets">
+
+<?php
+
+$id = 0; 
+foreach ($data as $dataset) { 
+  echo "<li class='dataset-card' id='dataset$id'> 
+  <h3>$dataset->title</h3> 
+  <h4><button id='description-button$id' class='description-button' onClick='displayDescription($id)'>Show description</button></h4>
+   <p class='description' id=$id>$dataset->description</p>
+    <h5 class='identifier'>Identifier: " . substr($dataset->identifier, -7) . " </h5> 
+    <h5>Date modified: $dataset->modified</h5>
+    <h5>Publisher/Point of contact: $dataset->contactpoint_fn</h5>";
+}
+
+?>
+
+ </ul>
+
+</body>
+</html>
